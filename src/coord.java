acf1495ac40b9ff2f22785acd91889e6edea9050
Qirion <gabriel.mfcorreia@gmail.com>
2013-06-08 13:53:35 -0300

    Up!

---
+++ src/coord.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+public class coord {
+	//Xf=(a*Xi+b)%m;
+	public static double seed = 1;
+	public static double a = 179424361;
+	public static double b = 11;
+	public static double m = 1000000000;
+	public static double dNom = 100000;
+	public static double[][] matriz;
+	
+	public static void main(String[] args){
+		msg("Programa de criação de coordenadas utilizando\nalgoritimos pseudo-randomicos");
+		double n = entrDouble("Entre com o número de estrelas a serem geradas");
+		matriz(matriz,(int)n);
+		
+		for(int c=0;c<matriz[0].length;c++){
+			for(int l=0;l<matriz.length;l++){
+				matriz[l][c] = 
+			}
+		}
+		
+	}//main
+	
+	//==============================================================
+	public static void matriz(double v[][],int n){
+		v = new double[n][4];
+	}//matriz
+	
+	public static double seeder(double Xi){
+		double Xf;
+		Xf = (a*Xi+b)%m;
+		Xf = (int)(Xf/dNom);
+		return Xf;
+	}//seeder
+	
+	public static void msg(String msg){
+		JOptionPane.showMessageDialog(null, msg);
+	}//msg
+	
+	public static double entrDouble(String msg){
+		return Double.parseDouble(JOptionPane.showInputDialog(msg));
+	}
+	
+}
